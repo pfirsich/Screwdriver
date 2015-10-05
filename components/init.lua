81c0f8535f0b5c70c6364338d81c052ace3a78ed
joelschum@gmail.com <joelschum@gmail.com>
2015-10-05 19:17:46 +0200

    Sorted notes. Moved some into source code.

--- components/init.lua
+++ components/init.lua
@@ -6,6 +6,12 @@ function addTable(tbl, add)
 	end 
 end 
 
+-- TODO: MetadataBoolean, MetadataString, MetadataNumber, MetadataChoice (choices = list of id-label pairs)
+-- Maybe leave these out, since there just a single gui element and adocate implementing own components for games, which can include these
+-- MetadataEntity? Draws connections (on-off-able) and has an edit mode to connect entities (maybe a dropdown?)
+
+-- FancyPolygon (two texture with configurable transition and a fan texture (e.g. for grass))	
+
 require "components.core"
 require "components.transforms"
 require "components.sprite"
